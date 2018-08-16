c86f5c90e0a0aa21297bc2c2f9371230bbc5fcc2
Konstantinos Kritikakis <kon.kri@outlook.com>
2018-08-16 22:13:41 +0300

    fixed metro stations error - sql unique key

--- AthEna/AthEna_WebApi/Controllers/AdminController.cs
+++ AthEna/AthEna_WebApi/Controllers/AdminController.cs
@@ -111,12 +111,13 @@ namespace AthEna_WebApi.Controllers
             }
             catch (Exception e)
             {
+                if (((System.Data.SqlClient.SqlException)e.InnerException).Number == 2627) // unique key violation from sql server...
+                    return StatusCode(400, _config["StatusCodesText:GeneralInputError"]);
                 return StatusCode(500, _config["StatusCodesText:ServerErr"]);
             }
         }
 
 
-        //hello jim
 
         [BasicAuthentication]
         [Route("api/Routes/{routeId?}")]
