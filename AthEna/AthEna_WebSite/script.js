f3f2cdfdc4d45e8830ee7c7bdd29b5ece490f73b
Kon Kri <kon.kri@outlook.com>
2018-09-01 18:44:47 +0300

    frontend: implemented post request with results + css fixes

--- AthEna/AthEna_WebSite/script.js
+++ AthEna/AthEna_WebSite/script.js
@@ -1,15 +1,17 @@
-function test(){
+//Declare config variables...
+var apiHostUrl = "http://athenaticket.ddns.net";
+
+function getContactsWithCardsAPI(){
     //Initiate call...
     $('#getContactsWithCards_TBody').append('<tr><td colspan="5"><br><center><h1><i class="fas fa-spinner fa-spin"></i></h1></td></tr>');
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "http://athenaticket.ddns.net/api/ContactWithCard",
+        "url": apiHostUrl + "/api/ContactWithCard",
         "method": "GET",
         "headers": {
           "Content-Type": "application/json",
           "Cache-Control": "no-cache",
-          "Postman-Token": "8931b777-7999-473d-ad2f-9d6352292007"
         }
       }
       
@@ -20,4 +22,37 @@ function test(){
             $('#getContactsWithCards_TBody').append('<tr><td>' + element.contact_FirstName + '</td><td>' + element.contact_LastName + '</td><td>' + element.contact_IdCardNum + '</td><td>' + element.card_RegisteredOn + '</td><td>' + element.card_ChargeExpiresOn + '</td></tr>');
         });
       });
+}
+
+function createNewContactWithCard(FirstName, LastName, IdCardNum, SocialSecurityNum, event){
+  //To prevent reloading in submitting form...
+  event.preventDefault();
+
+  //Initiate call...
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": apiHostUrl + "/api/ContactWithCard",
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache",
+    },
+    "processData": false,
+    "data": "{\n\t\"FirstName\":\"" + FirstName + "\",\n\t\"LastName\":\"" + LastName + "\",\n\t\"IdCardNum\":\"" + IdCardNum +"\",\n\t\"SocialSecurityNum\":" + SocialSecurityNum + "\n}"
+  }
+  var apiCall = 
+  //If create new attempt is sucessfull...
+  $.ajax(settings)
+  .done(function (response) {
+    alert("New Contact was created successfully and its associated Card. ContactId is \"" + response.newContactId + "\" and associated Card is \"" + response.newCardId +"\"");
+  })
+  .fail(function(jqXHR, status){
+    if (jqXHR.status == 400) {
+      alert("Bad Request. Please review your data.");
+    }else{
+      alert("An error occured.");
+    }    
+  });
+  
 }
