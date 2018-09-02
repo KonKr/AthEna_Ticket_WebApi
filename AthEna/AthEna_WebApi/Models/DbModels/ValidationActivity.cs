999de39a2ca878d77c23e557ebc85cfbd6ab7dfc
Dimitris Grevenos <dimgrev007@gmail.com>
2018-09-03 00:09:02 +0300

    added jsonIgnore

--- AthEna/AthEna_WebApi/Models/DbModels/ValidationActivity.cs
+++ AthEna/AthEna_WebApi/Models/DbModels/ValidationActivity.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace AthEna_WebApi.Models
 {
     public partial class ValidationActivity
     {
+        [JsonIgnore]
         public int Id { get; set; }
         public Guid VactivityId { get; set; }
         public DateTime ValidatedOn { get; set; }
@@ -14,9 +16,16 @@ namespace AthEna_WebApi.Models
         public Guid? RouteId { get; set; }
         public Guid CardId { get; set; }
 
+        [JsonIgnore]
         public Vehicle Bus { get; set; }
+
+        [JsonIgnore]
         public Card Card { get; set; }
+
+        [JsonIgnore]
         public Route Route { get; set; }
+
+        [JsonIgnore]
         public MetroStation Station { get; set; }
     }
 }
